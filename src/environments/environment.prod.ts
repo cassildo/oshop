ba36696d3eae2a8a5bb231ea856e42f9d5e2c8a4
Pedro Parreira <pmparreira@gmail.com>
2017-12-22 14:40:06 +0000

    project 2/7 - auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  	apiKey: "AIzaSyD_HyfdRBkW5MGmlPBz6Pe3h_bN07JhbMw",
+    authDomain: "oshop-39783.firebaseapp.com",
+    databaseURL: "https://oshop-39783.firebaseio.com",
+    projectId: "oshop-39783",
+    storageBucket: "",
+    messagingSenderId: "968368612044"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  	apiKey: "AIzaSyD_HyfdRBkW5MGmlPBz6Pe3h_bN07JhbMw",
+    authDomain: "oshop-39783.firebaseapp.com",
+    databaseURL: "https://oshop-39783.firebaseio.com",
+    projectId: "oshop-39783",
+    storageBucket: "",
+    messagingSenderId: "968368612044"
+  }
 };
